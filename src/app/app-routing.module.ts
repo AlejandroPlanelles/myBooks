21125208accb7a397c3b90ad18ad649966480ab1
AlejandroPlanelles <alejandro.planelles@gmail.com>
2023-05-23 17:19:40 +0200

    angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+const routes: Routes = [
+  {path:"home1", component: HomeComponent},
+  {path:"register1", component: RegisterComponent},
+  {path:"profile1", component: ProfileComponent},
+  {path:"**", component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
