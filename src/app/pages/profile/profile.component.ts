48d8876f2377c988ed8f3e42486c07a74dd96cdb
AlejandroPlanelles <alejandro.planelles@gmail.com>
2023-05-24 15:08:48 +0200

    reto2

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user'
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+public usuario:User
 
+constructor(){
+
+    this.usuario = new User("","","","","","")
+}
+  //   nombreCompleto():string{
+
+  //     return this.usuario.name + " " + this.usuario.last_name;
+  // }
+    mostrarNombre(nuevoNombre:string, nuevoApellido:string, nuevoEmail:string, nuevaFoto:string){
+
+      this.usuario.name=nuevoNombre;
+      this.usuario.last_name=nuevoApellido;
+      this.usuario.email=nuevoEmail;
+      this.usuario.photo=nuevaFoto;
+    }
 }
